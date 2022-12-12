e63f9a915d88719a0bc9117b53d5b21f51f6d57b
VeliAliyev <veli_aliyev@outlook.com>
2022-12-12 23:16:12 +0300

    initial commit

--- src/main/java/com/velialiyev/microservicesbreweryclient/MicroservicesBreweryClientApplication.java
+++ src/main/java/com/velialiyev/microservicesbreweryclient/MicroservicesBreweryClientApplication.java
@@ -1,9 +1,12 @@
 package com.velialiyev.microservicesbreweryclient;
 
+import com.velialiyev.microservicesbreweryclient.web.client.BreweryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BreweryClient.class)
 public class MicroservicesBreweryClientApplication {
 
     public static void main(String[] args) {
